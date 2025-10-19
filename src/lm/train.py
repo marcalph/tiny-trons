5cd83a4b1cbc98697e1345a1c13e8213ed41dcb3
marcalph <marcalph#protonmail.com>
2025-10-19 18:44:29 +0200

    add cuda device support

--- src/lm/train.py
+++ src/lm/train.py
@@ -16,6 +16,8 @@ def estimate_loss(dl, model):
         losses = torch.zeros(HPARAMS.eval_iters)
         for k in range(HPARAMS.eval_iters):
             X, Y = dl.get_batch(split)
+            X.to(HPARAMS.device)
+            Y.to(HPARAMS.device)
             _, loss = model(X, Y)
             losses[k] = loss.item()
             out[name] = losses.mean()
@@ -31,19 +33,26 @@ if __name__ == "__main__":
     train, val = ds.split()
     xb, yb = dl.get_batch(train)
     m = BigramLM(tok.vocab_sz)
+    m.to(HPARAMS.device)
     logits, loss = m(xb, yb)
     opt = AdamW(m.parameters(), lr=1e-3)
 
     # train loop
     for _ in range(HPARAMS.max_iters):
         xb, yb = dl.get_batch(ds.corpus)
+        xb.to(HPARAMS.device)
+        yb.to(HPARAMS.device)
         logits, loss = m(xb, yb)
-        opt.zero_grad(set_to_none=True)  # set.grad to None to save mem
+        opt.zero_grad(set_to_none=True)  # sets .grad to None to save mem
         loss.backward()
         opt.step()
         if _ % HPARAMS.eval_iters == 0:
             losses = estimate_loss(dl, m)
             logger.debug(
-                tok.decode(m.generate(torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0])
+                tok.decode(
+                    m.generate(
+                        torch.zeros((1, 1), dtype=torch.long).to(HPARAMS.device), max_new_tokens=100
+                    )[0]
+                )
             )
             logger.info(f"@step{_}: train loss {losses['train']}, val loss {losses['val']}")
